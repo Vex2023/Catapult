01fc694883042868f42fd8c8a4d36aba679dfdcf
zyklonbenjoyer <agootheclown@gmail.com>
2023-06-23 18:10:14 -0700

    Catapult robot code
    6 motor drivetrain; control catapult to load and fire acorn

---
+++ LICENSE/src/robot-config.cpp
@@ -0,0 +1,58 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor br = motor(PORT1, ratio18_1, true);
+motor bl = motor(PORT10, ratio18_1, false);
+motor ml = motor(PORT5, ratio18_1, false);
+motor mr = motor(PORT15, ratio18_1, true);
+motor fl = motor(PORT20, ratio18_1, false);
+motor fr = motor(PORT11, ratio18_1, true);
+motor cata = motor(PORT6, ratio36_1, false);
+controller Controller1 = controller(primary);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+// define variables used for controlling motors based on controller inputs
+bool Controller1XBButtonsControlMotorsStopped = true;
+
+// define a task that will handle monitoring inputs from Controller1
+int rc_auto_loop_function_Controller1() {
+  // process the controller input every 20 milliseconds
+  // update the motors based on the input values
+  while(true) {
+    if(RemoteControlCodeEnabled) {
+      // check the ButtonX/ButtonB status to control bl
+      if (Controller1.ButtonX.pressing()) {
+        bl.spin(forward);
+        Controller1XBButtonsControlMotorsStopped = false;
+      } else if (Controller1.ButtonB.pressing()) {
+        bl.spin(reverse);
+        Controller1XBButtonsControlMotorsStopped = false;
+      } else if (!Controller1XBButtonsControlMotorsStopped) {
+        bl.stop();
+        // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
+        Controller1XBButtonsControlMotorsStopped = true;
+      }
+    }
+    // wait before repeating the process
+    wait(20, msec);
+  }
+  return 0;
+}
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1);
+}
