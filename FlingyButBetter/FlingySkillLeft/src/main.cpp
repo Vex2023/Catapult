aa28f604c9a54bc107009d3cd8aa669973856e6b
zyklonbenjoyer <agootheclown@gmail.com>
2023-09-17 16:02:01 -0700

    updated matchLoadSideAuton_2, created goalSideAuton
    made slight adjustments to the match load side autonomous, and created the autonomous on goal side, getting 10 points by putting in 2 acorns into the goal if successful.

--- FlingyButBetter/FlingySkillLeft/src/main.cpp
+++ FlingyButBetter/FlingySkillLeft/src/main.cpp
@@ -12,7 +12,7 @@
 // [Name]               [Type]        [Port(s)]
 // br                   motor         20              
 // bl                   motor         11              
-// ml                   motor         5               
+// ml                   motor         16              
 // mr                   motor         15              
 // fl                   motor         1               
 // fr                   motor         10              
@@ -32,12 +32,12 @@ using namespace std;
 // A global instance of competition
 competition Competition;
 
-inertial Inertial = inertial(PORT2);
+inertial Inertial = inertial(PORT18);
 motor_group leftDrive (bl, ml, fl);
 motor_group rightDrive (br, mr, fr);
 smartdrive Flingy(leftDrive, rightDrive, Inertial, 12.56,10,9,distanceUnits::in);
 //for opening and closing claw
-bool clawClosed = false;
+bool clawClosed = true;
 // define your global instances of motors and other devices here
 
 /*---------------------------------------------------------------------------*/
@@ -147,23 +147,94 @@ void autonomous(void) {
 /*                                                                           */
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
-void closeClaw()
+void toggleClaw()
 {
+  cout<<"button presseed\n";
   if(!clawClosed)
   {
+     cout<<" before close\n";
     claw.spinToPosition(0,degrees);
+     cout<<" after close\n";
+
+    double current_position = claw.position(degrees);
+    cout<<"closed claw pos="<<current_position<<"\n";
     clawClosed = true;
   }
   else if(clawClosed)
   {
-    claw.spinToPosition(-78, degrees);
+     cout<<" before open\n";
+        claw.spinToPosition(78, degrees);
+        cout<<"after open\n";
     double current_position = claw.position(degrees);
-    cout<<"closed claw pos="<<current_position<<"\n";
+    cout<<"open claw pos="<<current_position<<"\n";
     // claw.spinFor(reverse, 70, degrees, 30, velocityUnits::pct);
     clawClosed = false;
   }
   cout << clawClosed << "R1 pressing" << endl;
 }
+void goLoadingPos() //make cata go down to loaded position
+{
+  double current_position = cata.position(degrees);
+  cout<<"start cata pos="<<current_position<<"\n";
+  //cata.spinToPosition(30,degrees);
+  cata.spinFor(reverse, 120, degrees, 40, velocityUnits::pct);
+  current_position = cata.position(degrees);
+  cout<<"cata loading pos="<<current_position<<"\n";
+  // cata.spinToPosition()
+}
+void shootFromLoadingPos() // make cata shoot from loaded position
+{
+  double current_position = cata.position(degrees);
+  cout<<"loaded cata pos ="<<current_position<<"\n";
+  //cata.spinToPosition(30,degrees);
+  cata.spinFor(reverse, 240, degrees, 40, velocityUnits::pct);
+  current_position = cata.position(degrees);
+  cout<<"cata after fire pos="<<current_position<<"\n";
+  // cata.spinToPosition()
+}
+void continuousShoot(){
+  wait(0.2,sec);
+  while(true)
+  {
+    if(Controller1.ButtonL1.pressing()){
+      break;
+    }
+    else
+    {
+      cata.spinFor(reverse, 360, degrees, 90, velocityUnits::pct);
+      double current_position = cata.position(degrees);
+      cout<<"pos after fire="<<current_position<<"\n";
+    }
+  }
+  wait(1.5,sec);
+}
+void printTemps()
+{
+      Brain.Screen.clearScreen();
+      Brain.Screen.setCursor(1,0);
+      Brain.Screen.print(" blT=");
+      Brain.Screen.print(bl.temperature(celsius));
+      Brain.Screen.print(" mlT=");
+      Brain.Screen.print(ml.temperature(celsius));
+      Brain.Screen.print(" flT=");
+      Brain.Screen.print(fl.temperature(celsius));
+      Brain.Screen.newLine();
+      Brain.Screen.print("brT=");
+      Brain.Screen.print(br.temperature(celsius));
+      Brain.Screen.print(" mrT=");
+      Brain.Screen.print(mr.temperature(celsius));
+      Brain.Screen.print(" frT=");
+      Brain.Screen.print(fr.temperature(celsius));
+      Brain.Screen.newLine();
+      Brain.Screen.print("CataT=");
+      Brain.Screen.print(cata.temperature(celsius));
+      Brain.Screen.newLine();
+      Brain.Screen.print("ClawT=");
+      Brain.Screen.print(claw.temperature(celsius));
+      Brain.Screen.print("Before");
+      Brain.Screen.print("After");
+}
+
 void usercontrol(void) {
   // User control code here, inside the loop
   while (1) {
@@ -195,10 +266,13 @@ void usercontrol(void) {
         frontBrake();
       else rearBrake();
     }
-
+    if (Controller1.ButtonA.pressing())
+    {
+      Flingy.driveFor(40, inches, 50, velocityUnits::pct);
+    }
 
     
-    if(Controller1.ButtonL1.pressing()){ //fire catapult
+    /*if(Controller1.ButtonL1.pressing()){ //fire catapult
       //cata.spinToPosition(-80,degrees);0
       //cata.setTimeout(2,seconds);
 
@@ -206,44 +280,18 @@ void usercontrol(void) {
       double current_position = cata.position(degrees);
       
       cout<<"pos after fire="<<current_position<<"\n";
-      /*l.spinToPosition(70, degrees);
-      grabby.driveFor(fwd, 2, inches);
-      wait(0.5,sec);*/
+      //l.spinToPosition(70, degrees);
+      //grabby.driveFor(fwd, 2, inches);
+      //wait(0.5,sec);
       //c.set(false);
-    } 
+    } */
 
-    if(Controller1.ButtonB.pressing()){ //fire catapult
-      
-      double current_position = cata.position(degrees);
-      cout<<"start cata pos="<<current_position<<"\n";
-      //cata.spinToPosition(30,degrees);
-      cata.spinFor(reverse, 120, degrees, 40, velocityUnits::pct);
-      current_position = cata.position(degrees);
-      cout<<"cata loading pos="<<current_position<<"\n";
-     // cata.spinToPosition()
-    }
 
-    if(Controller1.ButtonY.pressing()){//print all motor temperatures
-      Brain.Screen.clearScreen();
-      Brain.Screen.setCursor(1,0);
-      Brain.Screen.print(" blT=");
-      Brain.Screen.print(bl.temperature(celsius));
-      Brain.Screen.print(" mlT=");
-      Brain.Screen.print(ml.temperature(celsius));
-      Brain.Screen.print(" flT=");
-      Brain.Screen.print(fl.temperature(celsius));
-      Brain.Screen.newLine();
-      Brain.Screen.print("brT=");
-      Brain.Screen.print(br.temperature(celsius));
-      Brain.Screen.print(" mrT=");
-      Brain.Screen.print(mr.temperature(celsius));
-      Brain.Screen.print(" frT=");
-      Brain.Screen.print(fr.temperature(celsius));
-      Brain.Screen.newLine();
-      Brain.Screen.print("CataT=");
-      Brain.Screen.print(cata.temperature(celsius));
-    }
-    Controller1.ButtonR1.pressed(closeClaw);
+   
+   // Controller1.ButtonR1.pressed(toggleClaw); //this should not be in the while loop
+ 
+    //if(Controller1.ButtonR1.pressing())
+    //{ toggleClaw();}
    /* if(Controller1.ButtonR1.pressing())
     {
       if(!clawClosed)
@@ -278,7 +326,6 @@ void skill(){
   //Flingy.driveFor(fwd, 2, inches); //both wheels touch loading bar, more stable
   Flingy.drive(fwd, 1, velocityUnits::pct); //prevents wheels from bouncing off bar, pushes against bar
   for(int iter = 0; iter < 43; ++iter){
-    
     cout<< "iter= "<<iter<<"\n"; //print number of times fired
     fireCata(); //fires catapult
   }
@@ -289,21 +336,6 @@ void skill(){
   Starts with a preload in the fork
   Places preload into the goal
   Then, gets the green acorn in the middle into the goal*/
-void goalSideAuton(){
-  /*Pushes the preload into the goal*/
-  Flingy.driveFor(fwd, 44, inches, 50, velocityUnits::pct);
-  Flingy.setTurnVelocity(15, velocityUnits::pct);
-  Flingy.turnToHeading(90, degrees);
-  Flingy.setTimeout(2, sec);
-  Flingy.driveFor(fwd, 14, inches);
-
-  /*Get the middle green acorn and pushes it into the goal*/
-  Flingy.driveFor(reverse, 35, inches, 50, velocityUnits::pct);
-  Flingy.turnFor(vex::left, 35, degrees);
-  Flingy.driveFor(fwd, 15, inches, 50, velocityUnits::pct);
-  Flingy.turnFor(vex::right, 35, degrees);
-  Flingy.driveFor(fwd, 23, inches, 50, velocityUnits::pct); 
-}
 
 /***********************************************************************/
 /*Auton program for match load side
@@ -311,21 +343,91 @@ void goalSideAuton(){
   Shoots preload
   Rams green triball under the bar
   */
-void matchLoadSideAuton(){
-  fireCata();
-  Flingy.driveFor(reverse, 24, inches, 90, velocityUnits::pct);
+void matchLoadSideAuton_1(){
+  cata.spinFor(reverse, 120, degrees, 40, velocityUnits::pct);
+  Flingy.driveFor(reverse, 3, inches);
+  Flingy.driveFor(fwd, 8, inches);
+  cata.spinFor(reverse, 240, degrees, 40, velocityUnits::pct);
+  Flingy.turnFor(vex::right, 40, degrees);
+  Flingy.driveFor(reverse, 25, inches);
+  //Flingy.driveFor(fwd, 2, inches, 20, velocityUnits::pct);
+  //wait (1, sec);
+  //fireCata();
+  //Flingy.driveFor(reverse, 26, inches, 35, velocityUnits::pct);
 }
-//
+/***********************************************************************/
+/*
+autonomous to shoot the alliance acorn and remove acorn from match load zone, 
+push match load acorn and acorn under elevation bar to other side.
+touches elevation bar
+  */
+void matchLoadSideAuton_2(){
+  toggleClaw(); //open claw
+  cata.spinFor(reverse, 120, degrees, 40, velocityUnits::pct); //load catapult
+  Flingy.driveFor(reverse, 2, inches, 20, velocityUnits::pct); //shake claw
+  cata.spinFor(reverse, 240, degrees, 40, velocityUnits::pct); //shoot catapult
+  Flingy.driveFor(fwd, 2, inches, 10, velocityUnits::pct);// move into to touch load zone
+  claw.setTimeout(1, sec);
+  toggleClaw(); //grab acorn
+  Flingy.driveFor(reverse, 5, inches, 20, velocityUnits::pct); //take acorn out of match load zone
+  Flingy.setTurnVelocity(10, velocityUnits::pct);
+  Flingy.turnToHeading(260,degrees); // turn to diagnal
+  Flingy.driveFor(fwd, 11, inches, 30, velocityUnits::pct);
+
+
+  Flingy.turnToHeading(225,degrees); //facing horizontal bar
+  toggleClaw(); //open claw
+  Flingy.driveFor(fwd, 16, inches, 50, velocityUnits::pct); // push acorn to other side
+}
+
+void goalSideAuton() {
+  Flingy.setTurnVelocity(15, velocityUnits::pct);
+  toggleClaw();//open claw
+  Flingy.driveFor(reverse, 2, inches, 40, velocityUnits::pct); //shake claw
+  Flingy.driveFor(fwd, 2, inches, 40, velocityUnits::pct);
+  toggleClaw();//close claw
+  Flingy.driveFor(fwd, 32, inches, 40, velocityUnits::pct);
+  Flingy.turnToHeading(80, deg);
+  Flingy.driveFor(fwd, 6, inches, 50, velocityUnits::pct);
+  toggleClaw();
+  Flingy.driveFor(reverse, 10.5, inches, 40, velocityUnits::pct);
+  Flingy.turnToHeading(235, deg);
+  wait(0.5,sec);
+  Flingy.driveFor(fwd, 7, inches, 40, velocityUnits::pct);
+  claw.setTimeout(0.5, sec);
+  toggleClaw();
+  Flingy.driveFor(reverse, 5, inches, 40, velocityUnits::pct);
+  Flingy.turnToHeading(90, deg);
+  Flingy.driveFor(fwd, 12, inches, 40, velocityUnits::pct);
+  toggleClaw();
+  Flingy.driveFor(reverse, 12, inches, 40, velocityUnits::pct);
+ // Flingy.driveFor(fwd, 6, inches, 40, velocityUnits::pct);
+  //toggleClaw();
+  //Flingy.driveFor(reverse, 5, inches, 40, velocityUnits::pct);
+  //Flingy.turnToHeading(10, deg);
+  //Flingy.driveFor(reverse, 5, inches, 40, velocityUnits::pct);
+ // Flingy.turnToHeading(90, deg);
+  //toggleClaw();
+  //Flingy.driveFor(reverse, 10, inches, 40, velocityUnits::pct);
+  //Flingy.turnToHeading(270, deg);
+  //Flingy.driveFor(reverse, 20, inches, 50, velocityUnits::pct);
+}
 // Main will set up the competition functions and callbacks.
 //
 int main() {
   pre_auton();
   // Set up callbacks for autonomous and driver control periods.
-  Competition.autonomous(skill); //slot 1
+  Controller1.ButtonR1.pressed(toggleClaw);
+  Controller1.ButtonY.pressed(printTemps);
+  Controller1.ButtonB.pressed(goLoadingPos);
+  Controller1.ButtonX.pressed(shootFromLoadingPos);
+  Controller1.ButtonL1.pressed(continuousShoot);
+  //Competition.autonomous(skill); //slot 1
   //Competition.autonomous(goalSideAuton); //slot 2
-  //Competition.autonomous(matchLoadSideAuton); //slot 3
-  //Competition.autonomous(fireCata); //slot 3
+  Competition.autonomous(matchLoadSideAuton_1); //slot 3
+  //Competition.autonomous(matchLoadSideAuton_2); //slot 4
 
+  //Competition.autonomous(fireCata); //slot 3
   Competition.drivercontrol(usercontrol);
 
   // Run the pre-autonomous function.
